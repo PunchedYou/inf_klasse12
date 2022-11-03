5d3134320051e2648230a7325072c11d83a269eb
PunchedYou <nibot04@gmail.com>
2022-11-03 17:16:00 +0100

    added new mause code

--- src/dstrukturen/maeuse/Test.java
+++ src/dstrukturen/maeuse/Test.java
@@ -9,16 +9,16 @@ public class Test {
         Test t = new Test();
         t.kampfTesten(); // Kampf wird mit mit festen Stärken getestet
 
-        Kampf k = new Kampf();
-        k.zufallsBelegung(5);
-        k.kampfAusfuehren(); // Kampf wird mit zufällligen Stärken getestet
+        MausKampf mk = new MausKampf();
+        mk.zufallsBelegung(5);
+        mk.kampfAusfuehren(); // Kampf wird mit zufällligen Stärken getestet
 
         int[] arr1 = { 7, 6, 5, 4, 3, 2, 8, 9, 1 }; // Ja
-        k.reihenfolgeSimulieren(arr1);
+        mk.reihenfolgeSimulieren(arr1);
         int[] arr2 = { 1, 3, 4, 6, 7, 10, 9, 11, 12, 8, 5, 2 }; // Ja
-        k.reihenfolgeSimulieren(arr2);
+        mk.reihenfolgeSimulieren(arr2);
         int[] arr3 = { 4, 5, 3, 1, 2, 6 }; // Nein
-        k.reihenfolgeSimulieren(arr3); // Eine vorgegebene Reihenfolge wird getestet/simuliert
+        mk.reihenfolgeSimulieren(arr3); // Eine vorgegebene Reihenfolge wird getestet/simuliert
 
         final long timeEnd = System.currentTimeMillis();
         System.out.println("Zeit des Programms: " + (timeEnd - timeStart) + "ms");
@@ -26,14 +26,14 @@ public class Test {
 
     public void kampfTesten() {
         int[] arr = { 10, 5, 1, 7, 4 }; // Hier Stärken der Mäuse angeben
-        Queue<Maus> q = new Queue<>();
+        Queue<Maus> q = new Queue<Maus>();
 
         for (int i = 0; i < arr.length; i++) {
             Maus m = new Maus(i + 1, arr[i]);
             q.enqueue(m);
         }
 
-        Kampf k = new Kampf();
+        MausKampf k = new MausKampf();
         k.manuelleBelegung(q);
         k.kampfAusfuehren();
     }
